88fce7884d59e56dd0c72517f994426dea6440aa
Amy <amycheck8715@gmail.com>
2023-07-01 11:15:38 -0400

    started tasklist

---
+++ app/routes/tasklist.tsx
@@ -0,0 +1,37 @@
+import type { V2_MetaFunction } from "@remix-run/node";
+import { Reorder } from 'framer-motion';
+import Sidebar from '~/components/Sidebar';
+import Navbar from '~/components/Navbar';
+import { useState } from "react";
+
+export const meta: V2_MetaFunction = () => {
+  return [
+    { title: "Task List" },
+    { name: "description", content: "Create or accept a task here" },
+  ];
+};
+
+export default function Index() {
+  const [tasks, setTasks] = useState(['1', '2', '3', '4', '5', '6', '7', '8', '9']);
+
+  return (
+    <div className="flex bg-black w-full min-h-full h-screen">
+      <Sidebar />
+      <div className="flex-1">
+        <Navbar />
+        <Reorder.Group
+            className="flex-col space-y-1 space-x-4 flex-wrap"
+            onReorder={setTasks}
+            values={tasks}
+            
+            >
+            {tasks.map((task) => (
+                <Reorder.Item  drag key={task} value={task} className="bg-white rounded-lg cursor-grab p-4">
+                    {task}
+                </Reorder.Item>
+            ))}
+        </Reorder.Group>
+      </div>
+    </div>
+  );
+}
