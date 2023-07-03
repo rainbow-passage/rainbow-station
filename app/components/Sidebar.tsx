d65a5ce9f7d04459b5d7b97abc23532fb0d022b0
Amy <amycheck8715@gmail.com>
2023-07-03 09:28:07 -0400

    workin on task list

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -9,7 +9,7 @@ const Sidebar: React.FC = () => {
       <div className="fixed w-64.5 bg-backg h-12 " />
       <div className="relative w-64.5 mt-12 bg-shad">
         <div className="w-64 bg-backg h-screen">
-          <NavLink to="/tasklist" className="h-4 w-60 bg-shad text-white">
+          <NavLink to="/tasklist" className="w-60 h-4  bg-shad text-white">
             Task List
           </NavLink>
         </div>
